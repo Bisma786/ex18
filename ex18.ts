30da5150f2ffa67eaaa7ad7f453cdcd80a6b42fb
Bisma Gondal <159830145+Bisma786@users.noreply.github.com>
2024-02-26 21:49:40 +0500

    Add files via upload

---
+++ ex18.ts
@@ -0,0 +1,57 @@
+// Seeing the World: Think of at least five places in the world you’d like to visit.
+// • Store the locations in a array. Make sure the array is not in alphabetical order.
+
+// • Print your array in its original order.
+
+// • Print your array in alphabetical order without modifying the actual list.
+
+// • Show that your array is still in its original order by printing it.
+
+// • Print your array in reverse alphabetical order without changing the order of the original list.
+
+// • Show that your array is still in its original order by printing it again.
+
+// • Reverse the order of your list. Print the array to show that its order has changed.
+
+// • Reverse the order of your list again. Print the list to show it’s back to its original order.
+
+// • Sort your array so it’s stored in alphabetical order. Print the array to show that its order has been changed.
+
+// • Sort to change your array so it’s stored in reverse alphabetical order. Print the list to show that its order has changed.
+
+let destinations: string[]=["Makkah", "Madina","Tokyo", "Antartica", "Swat"]
+
+console.log("Orignal Order:")
+console.log(destinations);
+
+console.log("Alphabetical order without modifying the actual list")
+console.log([...destinations].sort())
+
+console.log("\n Showing array is still in its orignal order")
+console.log(destinations);
+
+console.log("\n Reverse Alphabetical order without modifying the actual list")
+console.log([...destinations].sort().reverse())
+
+console.log("\n Showing that array is still in its orignal form")
+console.log(destinations)
+
+console.log("\n Reverse Order")
+destinations.reverse()
+console.log(destinations)
+
+console.log("\n Back To Orignal")
+destinations.reverse()
+console.log(destinations)
+
+console.log("\n Change to Alphabetical Order")
+destinations.sort()
+console.log(destinations)
+
+
+console.log("\n Change to Reverse Alphabetical Order")
+destinations.sort().reverse()
+console.log(destinations)
+
+
+
